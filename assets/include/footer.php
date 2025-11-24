5dced4f86f42ce67cc80330c4ad53b760087faad
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-11-24 22:33:28 +0100

    Suppression du lien vers Instagram
    Le logo ne s'affiche pas, la ressource est marquée comme inexistante

--- assets/include/footer.php
+++ assets/include/footer.php
@@ -12,8 +12,6 @@
             <a href="https://discord.com/"><img class="logo-marque" src="assets/img/discord-logo.png" alt="Logo de Discord"></a>
             <a href="https://facebook.com/"><img class="logo-marque" src="assets/img/facebook-logo.png" alt="Logo de Facebook"></a>
             <a href="https://x.com/"><img class="logo-marque" src="assets/img/x-logo.png" alt="Logo de X, ancien Twitter"></a>
-            <!-- <a href="https://instagram.com"><img class="logo-marque" src="assets/img/instragram-logo.png" alt="Logo Instagram"></a> -->
-            <a href="https://instagram.com/"><img src="assets/img/instagram-logo.png" alt="Logo de Instagram"></a>
         </div>
     </div>
 </footer>
