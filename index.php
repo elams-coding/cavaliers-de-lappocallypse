388a8e41268312dcf57a28614893149c28243d95
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-01 15:44:43 +0100

    Changement des blocs d'articles en lien externe

--- index.php
+++ index.php
@@ -42,86 +42,100 @@
 
         <article>
             <div id="container-articles">
-                <div class="box-article">
-                    <h2>Annonce du tournoi à venir &laquo; Open d'Automne &raquo;</h2>
-                    <img class="img-article" src="assets/img/chess in front of computer.jpg"
-                        alt="Illustration de pions d'un plateau d'échecs en face d'un écran d'ordinateur">
-                    <div class="text-content">
-                        <p class="summary">Le club prépare l'&laquo; Open d'Automne &raquo;, un événement ouvert à tous
-                            les niveaux qui se tiendra le mois prochain.
-                        </p>
-                        <p>Les inscriptions sont déjà ouvertes et les places limitées.
-                            <br><span class="know-more">En savoir plus</span>
-                        </p>
-                        <span class="date-article">15/11/2025</span>
+                <a href="asstes/pages/articles/index.php">
+                    <div class="box-article">
+                        <h2>Annonce du tournoi à venir &laquo; Open d'Automne &raquo;</h2>
+                        <img class="img-article" src="assets/img/tournoi d'échecs.avif"
+                            alt="Plateau d'échecs avec des pions en bois">
+                        <div class="text-content">
+                            <p class="summary">Le club prépare l'&laquo; Open d'Automne &raquo;, un événement ouvert à
+                                tous
+                                les niveaux qui se tiendra le mois prochain.
+                            </p>
+                            <p>Les inscriptions sont déjà ouvertes et les places limitées.
+                                <br><span class="know-more">En savoir plus</span>
+                            </p>
+                            <span class="date-article">15/11/2025</span>
+                        </div>
                     </div>
-                </div>
+                </a>
 
-                <div class="box-article">
-                    <h2>Lancement d'ateliers pour les débutants</h2>
-                    <img class="img-article" src="assets/img/chess in front of computer.jpg"
-                        alt="Illustration de pions d'un plateau d'échecs en face d'un écran d'ordinateur">
-                    <div class="text-content">
-                        <p class="summary">De nouveaux ateliers hebdomadaires sont proposés pour aider les débutants à
-                            comprendre les principes fondamentaux des ouvertures et finales.</p>
-                        <p class="summary">Ces sessions seront animées par les entraîneur bénévoles du club.
-                            <br><span class="know-more">En savoir plus</span>
-                        </p>
-                        <span class="date-article">27/10/2025</span>
+                <a href="assets/pages/articles/index.php">
+                    <div class="box-article">
+                        <h2>Lancement d'ateliers pour les débutants</h2>
+                        <img class="img-article" src="assets/img/plateau d'échecs.avif"
+                            alt="Plateau d'échecs en verre avec des pions en verre">
+                        <div class="text-content">
+                            <p class="summary">De nouveaux ateliers hebdomadaires sont proposés pour aider les débutants
+                                à
+                                comprendre les principes fondamentaux des ouvertures et finales.</p>
+                            <p class="summary">Ces sessions seront animées par les entraîneur bénévoles du club.
+                                <br><span class="know-more">En savoir plus</span>
+                            </p>
+                            <span class="date-article">27/10/2025</span>
+                        </div>
                     </div>
-                </div>
+                </a>
 
-                <div class="box-article">
-                    <h2>Accueil d'un maître international pour une séance de simultanée</h2>
-                    <img class="img-article" src="assets/img/chess in front of computer.jpg"
-                        alt="Illustration de pions d'un plateau d'échecs en face d'un écran d'ordinateur">
-                    <div class="text-content">
-                        <p class="summary">Le club recevra prochainement un maître international pour une séance
-                            exceptionnelle de parties simultanées.
-                        </p>
-                        <p class="summary">Cet événement rare offrira aux membres une occasion unique de tester leur
-                            niveau face à un joueur d'élite.
-                            <br>
-                            <span class="know-more">En savoir plus</span>
-                        </p>
-                        <span class="date-article">31/08/2024</span>
+                <a href="assets/pages/articles/index.php">
+                    <div class="box-article">
+                        <h2>Accueil d'un maître international pour une séance de simultanée</h2>
+                        <img class="img-article" src="assets/img/maître d'échecs.avif"
+                            alt="Maître d'échecs qui joue en simultanée avec des jeunes joueurs">
+                        <div class="text-content">
+                            <p class="summary">Le club recevra prochainement un maître international pour une séance
+                                exceptionnelle de parties simultanées.
+                            </p>
+                            <p class="summary">Cet événement rare offrira aux membres une occasion unique de tester leur
+                                niveau face à un joueur d'élite.
+                                <br>
+                                <span class="know-more">En savoir plus</span>
+                            </p>
+                            <span class="date-article">31/08/2024</span>
+                        </div>
                     </div>
-                </div>
+                </a>
 
-                <div class="box-article">
-                    <h2>Retour sur le tournoi &laquo; Gambit de Printemps &raquo;</h2>
-                    <img class="img-article" src="assets/img/chess in front of computer.jpg"
-                        alt="Illustration de pions d'un plateau d'échecs en face d'un écran d'ordinateur">
-                    <div class="text-content">
-                        <p class="summary">
-                            Le tournoi annuel &laquo; Gambit de Printemps &raquo; a réuni plus 60 joueurs dans une
-                            ambiance conviviale et compétitive.
-                        </p>
-                        <p>Les jeunes talents du club y ont particulièrement brillé avec plusieurs podiums.
-                            <br><span class="know-more">En savoir plus</span>
-                        </p>
-                        <span class="date-article">13/06/2024</span>
+                <a href="assets/pages/articles/index.php">
+                    <div class="box-article">
+                        <h2>Retour sur le tournoi &laquo; Gambit de Printemps &raquo;</h2>
+                        <img class="img-article" src="assets/img/échecs partie.avif"
+                            alt="Partie d'échecs au tournoi Gambit de Printemps">
+                        <div class="text-content">
+                            <p class="summary">
+                                Le tournoi annuel &laquo; Gambit de Printemps &raquo; a réuni plus 60 joueurs dans une
+                                ambiance conviviale et compétitive.
+                            </p>
+                            <p>Les jeunes talents du club y ont particulièrement brillé avec plusieurs podiums.
+                                <br><span class="know-more">En savoir plus</span>
+                            </p>
+                            <span class="date-article">13/06/2024</span>
+                        </div>
                     </div>
-                </div>
+                </a>
 
-                <div class="box-article">
-                    <h2>Nouveau site web disponible</h2>
-                    <img class="img-article" src="assets/img/chess in front of computer.jpg"
-                        alt="Illustration de pions d'un plateau d'échecs en face d'un écran d'ordinateur">
-                    <div class="text-content">
-                        <p class="summary">Ô jeune pousse, les Cavaliers de l'apocalypse vous présente leur nouveau site
-                            web.
-                            <br>Nouveau, moderne et tout frais !
-                        </p>
-                        <p class="summary">Vous pourrez consulter les actualités, obtenir des informations sur le club
-                            et
-                            ses différents memebres.
-                            <br>
-                            <span class="know-more">En savoir plus</span>
-                        </p>
-                        <span class="date-article">02/03/2018</span>
+                <a href="assets/pages/articles/nouveau-site-web.php" target="_self">
+                    <div class="box-article">
+                        <h2>Nouveau site web disponible</h2>
+                        <img class="img-article" src="assets/img/chess in front of computer.jpg"
+                            alt="Illustration de pions d'un plateau d'échecs en face d'un écran d'ordinateur">
+                        <div class="text-content">
+                            <p class="summary">Ô jeune pousse, les Cavaliers de l'apocalypse vous présente leur nouveau
+                                site
+                                web.
+                                <br>Nouveau, moderne et tout frais !
+                            </p>
+                            <p class="summary">Vous pourrez consulter les actualités, obtenir des informations sur le
+                                club
+                                et
+                                ses différents memebres.
+                                <br>
+                                <span class="know-more">En savoir plus</span>
+                            </p>
+                            <span class="date-article">02/03/2018</span>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
         </article>
     </main>
