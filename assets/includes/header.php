72bda77e2592f29f7a8115fc6e53c83a59d1dcc5
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-01 15:41:48 +0100

    La page ne change pas pour le logo header

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -1,6 +1,6 @@
 <header>
     <!-- (En-tête) Bannière du site web -->
-    <a href="/index.php">
+    <a href="/index.php" target="_self">
         <img id="logo" src="assets/img/logo/logo.png" alt="Logo du club d'échecs : Cavaliers de l'apocalypse">
     </a>
     <h1>Cavaliers&NonBreakingSpace;de l'apocalypse</h1>
