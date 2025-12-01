6808cfc7bc6d164f718b0b24d8db19c0a7127354
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-01 17:56:24 +0100

    Page d'article fini

--- assets/pages/articles/nouveau-site-web.php
+++ assets/pages/articles/nouveau-site-web.php
@@ -4,10 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description"
-        content="Site Web du club d'échec les Cavaliers de l'apocallypse. Ici, vous trouverez les actualités de votre club d'échecs.">
+    <meta name="description" content="Site Web du club d'échec les Cavaliers de l'apocallypse.">
     <meta http-equiv="X-UA-Compatible" content="IE=7">
-    <title>Déploiement du nouveau site web - Les Cavaliers de l'apocallypse</title>
+    <title>Déploiement du nouveau site - Les Cavaliers de l'apocallypse</title>
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -28,6 +27,39 @@
 
     <main>
         <!-- Contenu principal de la page -->
+        <h2>Déploiement du nouveau site</h2>
+
+        <div class="contenu-article">
+            <img class="img-article" src="../../img/chess in front of computer.jpg"
+                alt="Illustration de pions d'un plateau d'échecs en face d'un écran d'ordinateur">
+
+            <div class=>
+                <p class="en-tete-texte">
+                    Ô jeune pousse, les Cavaliers de l'apocallypse vous présente leur nouveau site&NonBreakingSpace;web.
+                    <br>Nouveau, moderne et tout frais !
+                </p>
+                <p>
+                    C'est avec un grand plaisir que nous vous annonçons l'ouverture de notre site internet, ouvert à
+                    tous publics.
+                    <br>
+                    Vous y trouverez :
+                    <br>
+                <ul>
+                    <li>les actualités du club</li>
+                    <li>les informations du club</li>
+                    <li>la liste des membres</li>
+                    <li>une page de contact afin de nous contacter</li>
+                </ul>
+                </p>
+                <p>
+                    Que vous soyez débutant curieux ou joueur confirmé, ce site est conçu pour vous offrir un lieu de
+                    découverte autour du jeu d'échecs.
+                    <br>
+                    Explorez les plateaux, participez aux tournois et rejoignez-nous dans cette belle aventure
+                    stratégique !
+                </p>
+            </div>
+        </div>
     </main>
 
     <?php include '../../includes/footer.php'; ?>
