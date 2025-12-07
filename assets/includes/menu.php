a8e09f599db810a860cef759a5c1c73f9320e633
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-07 22:01:05 +0100

    page infos du club reste dans le même onglet

--- assets/includes/menu.php
+++ assets/includes/menu.php
@@ -6,7 +6,7 @@
 
     <ul>
         <li><a href="../../#title-section-articles" target="_self">Articles</a></li>
-        <li><a href="assets/pages/club_infos.php" target="_blank">Infos&NonBreakingSpace;du&NonBreakingSpace;club et
+        <li><a href="assets/pages/club_infos.php" target="_self">Infos&NonBreakingSpace;du&NonBreakingSpace;club et
                 membres</a></li>
         <li><a href="assets/pages/contact.php" target="_blank">Contact</a></li>
     </ul>
