b0baac6650d878ab060d1934000d8b5e489b78e9
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-07 22:00:04 +0100

    Ajout de footer2

---
+++ assets/includes/footer2.php
@@ -0,0 +1,59 @@
+<footer>
+    <!-- Pied de page  -->
+    <div>
+        <div id="box-social-media" class="footer-sub-boxes">
+            <h3>Nous retrouver sur nos réseaux</h3>
+
+            <ul>
+                <li>
+                    <a href="https://discord.com" target="_blank" rel="noreferrer noopener">
+                        <img class="logo-marque" src="/assets/img/logo/discord-logo.png" alt="Logo de Discord">
+                        <span>Discord</span>
+                    </a>
+                </li>
+
+                <hr>
+
+                <li>
+                    <a href="https://facebook.com" target="_blank" rel="noreferrer noopener">
+                        <img class="logo-marque" src="/assets/img/logo/facebook-logo.png" alt="Logo de Facebooks">
+                        <span>Facebook</span>
+                    </a>
+                </li>
+
+                <hr>
+
+                <li>
+                    <a href="https://x.com" target="_blank" rel="noreferrer noopener">
+                        <img class="logo-marque" src="/assets/img/logo/x-logo.png" alt="Logo de X">
+                        <span>X</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <hr>
+
+        <div id="box-legal-notices" class="footer-sub-boxes">
+            <h3>Mentions légales</h3>
+            <ul>
+                <li>
+                    <a href="assets/pages/mentions_legales.php" target="_blank">Lien vers les
+                        mentions&NonBreakingSpace;légales</a>
+                </li>
+                <li>
+                    <a class="download-link" href="assets/ressources/mentions_legales.pdf"
+                        download="mentions_legales.pdf">Télécharger&NonBreakingSpace;les&NonBreakingSpace;mentions&NonBreakingSpace;légales</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div id="last-lines">
+        <span>Cavalier&NonBreakingSpace;de&NonBreakingSpace;l'apocallypse</span>
+        <span>
+            &copy;&NonBreakingSpace;<?php echo date("Y") ?>
+            Tous&NonBreakingSpace;droits&NonBreakingSpace;réservés.
+        </span>
+    </div>
+</footer>
