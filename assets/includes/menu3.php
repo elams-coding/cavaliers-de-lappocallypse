3cc3c6bcc5741e4d36d8de444db947d4ea42fa5e
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-07 23:57:47 +0100

    3ème menu pour éviter d'avoir le lien contact vers lui-même

---
+++ assets/includes/menu3.php
@@ -0,0 +1,13 @@
+<section class="nav-bar">
+    <!-- Section menu -->
+    <h3>Menu</h3>
+
+    <hr>
+
+    <ul>
+        <li><a href="/index.php" target="_self">Accueil</a></li>
+        <li><a href="/assets/pages/club_infos.php" target="_self">Infos&NonBreakingSpace;du&NonBreakingSpace;club et
+                membres</a></li>
+        <li><a href="/assets/pages/mentions_legales.php" target="_blank">Mentions légales</a></li>
+    </ul>
+</section>
